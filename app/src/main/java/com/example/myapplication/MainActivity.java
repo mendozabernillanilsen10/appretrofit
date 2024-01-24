0da56c765fccddf851599f21c90220f0289d9755
MENDOZA <44247358+mendozabernillanilsen10@users.noreply.github.com>
2024-01-24 14:37:20 -0500

    S
    S

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,26 +1,20 @@
 package com.example.myapplication;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.annotation.SuppressLint;
-import android.database.sqlite.SQLiteDatabase;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.QuickContactBadge;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import com.example.myapplication.Cliente.conn;
 import com.example.myapplication.Service.Service;
-import com.example.myapplication.adapter.TrabajadorAdapter;
 import com.example.myapplication.db.DatabaseHelper;
-import com.example.myapplication.db.TrabajadorDataSource;
 import com.example.myapplication.model.Trabajador;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
@@ -29,83 +23,91 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView.Adapter adapterPopular;
-    private SweetAlertDialog loadingDialog;
 
     private Button filledTonalButton;
     private TextView textView10;
     private RecyclerView recyclerViewPopular;
-    @SuppressLint("MissingInflatedId")
+    private SweetAlertDialog loadingDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView10 = findViewById(R.id.textView10);
         filledTonalButton = findViewById(R.id.filledTonalButton);
-
         filledTonalButton.setOnClickListener(v -> {
-            initRecyclerView();
+            new DownloadAndSaveTask().execute();
         });
     }
 
-    private void initRecyclerView() {
-        recyclerViewPopular = findViewById(R.id.view1);
-        recyclerViewPopular.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+    private class DownloadAndSaveTask extends AsyncTask<Void, Void, List<Trabajador>> {
+        private long startTime;
+        private long downloadTime;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            loadingDialog = new SweetAlertDialog(MainActivity.this, SweetAlertDialog.PROGRESS_TYPE);
+            loadingDialog.setCancelable(false);
+            loadingDialog.setTitleText("Sincronizando...");
+            loadingDialog.show();
+            startTime = System.currentTimeMillis();
+        }
+        @Override
+        protected List<Trabajador> doInBackground(Void... voids) {
 
-        loadingDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
-        loadingDialog.setCancelable(false);
-        loadingDialog.setTitleText("Sincronizando...");
-        loadingDialog.show();
-        Service ser;
-        ser = conn.Mediador();
-        long startTime = System.currentTimeMillis();
+            Service ser = conn.Mediador();
+            Call<List<Trabajador>> call = ser.obtenerTrabajadores();
 
-        Call<List<Trabajador>> call =   ser.obtenerTrabajadores();
-        call.enqueue(new Callback<List<Trabajador>>() {
-            @Override
-            public void onResponse(Call<List<Trabajador>> call, Response<List<Trabajador>> response) {
-                long endTime = System.currentTimeMillis();
-                long elapsedTime = endTime - startTime;
-
-                loadingDialog.setCancelable(true);
-                loadingDialog.cancel();
-
-                if(response.isSuccessful()) {
-                    List<Trabajador> trabajadores = response.body();
-                    saveTrabajadores(trabajadores);
-                }else{
+            try {
+                Response<List<Trabajador>> response = call.execute();
+                if (response.isSuccessful()) {
+                    return response.body();
                 }
-
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            @Override
-            public void onFailure(Call<List<Trabajador>> call, Throwable t) {
-                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-    }
-    public void saveTrabajadores(List<Trabajador> trabajadores) {
-        DatabaseHelper dataSource = new DatabaseHelper(this, "db", null, 1);
-        SQLiteDatabase database = dataSource.getWritableDatabase();
-        database.beginTransaction();
-        try {
-            for (Trabajador trabajador : trabajadores) {
-                database.insert("usuarios", null, trabajador.toValues());
-            }
-            database.setTransactionSuccessful();
-            textView10.setText("Se guardaron " + trabajadores.size() + " trabajadores");
-
-        } catch (Exception e) {
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-        } finally {
-            database.endTransaction();
+            return null;
         }
 
+        @Override
+        protected void onPostExecute(List<Trabajador> trabajadores) {
+            super.onPostExecute(trabajadores);
+
+            loadingDialog.cancel();
+            if (trabajadores != null) {
+                saveTrabajadores(trabajadores);
+            } else {
+                // Manejo de errores en la respuesta no exitosa
+                Toast.makeText(MainActivity.this, "Error en la descarga", Toast.LENGTH_SHORT).show();
+            }
+        }
+        private void saveTrabajadores(List<Trabajador> trabajadores) {
+            DatabaseHelper dataSource = new DatabaseHelper(MainActivity.this, "db_taaSSS", null, 1);
+            SQLiteDatabase database = dataSource.getWritableDatabase();
+            database.beginTransaction();
+            try {
+                for (Trabajador trabajador : trabajadores) {
+                    database.insert("usuarios", null, trabajador.toValues());
+                }
+                database.setTransactionSuccessful();
+                long insertEndTime = System.currentTimeMillis();
+                long insertTime = insertEndTime - startTime;
+
+                long insertTimeInSeconds = insertTime / 1000;
+                long totalTimeInSeconds = (downloadTime + insertTime) / 1000;
+                // Convertir tiempos a minutos
+                long insertTimeInMinutes = insertTimeInSeconds / 60;
+                long totalTimeInMinutes = totalTimeInSeconds / 60;
+
+                textView10.setText("Descarga: " + downloadTime + "ms, " +
+                        "Inserción: " + insertTimeInSeconds + "s (" + insertTimeInMinutes + "min), " +
+                        "Total: " + totalTimeInSeconds + "s (" + totalTimeInMinutes + "min), " +
+                        "Se guardaron " + trabajadores.size() + " trabajadores");
+            } catch (Exception e) {
+                Toast.makeText(MainActivity.this, "Error al guardar: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            } finally {
+                database.endTransaction();
+            }
+        }
     }
-
-
-
-
-
 }
--- app/src/main/java/com/example/myapplication/Service/Service.java
+++ app/src/main/java/com/example/myapplication/Service/Service.java
@@ -8,9 +8,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface Service {
-    @GET("ListTrabajadores2021.php?iddatabase=AGROVISIONTEAM&iddatabase2=AGROVISIONCORP&idempresa=001")
+    @GET("GetQuery.php?iddatabase=AGROVISIONTEAM&query=exec GetListTrabTotal_mobile 'AGROVISIONCORP'")
     Call<List<Trabajador>> obtenerTrabajadores();
-
-
 
 }
